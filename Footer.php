692ffb42f1e597b6692569bda3c56ecfd42a110e
BKM-2021 <93521358+BKM-2021@users.noreply.github.com>
2022-03-09 12:21:25 +0000

    commit

---
+++ Footer.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+    <style>
+    .fa {
+    padding: 20px;
+    font-size: 30px;
+    width: 50px;
+    text-align: center;
+    text-decoration: none;
+    margin: 5px 2px;
+    }
+
+    .fa:hover {
+        opacity: 0.7;
+    }
+
+    .fa-facebook {
+    background: #3B5998;
+    color: white;
+    }
+
+    .fa-twitter {
+    background: #55ACEE;
+    color: white;
+    }
+
+    .fa-instagram {
+    background: #EA0C5F;
+    color: white;
+    }
+    </style>
+</head>
+<body>
+    
+</body>
+<footer class="page-footer font-small bg-light-grey lighten-3 pt-4">
+
+  <div class="container text-center text-md-left">
+    <div class="row">
+      <div class="col-md-4 col-lg-3 ms-auto my-md-4 my-0 mt-4 mb-1">
+        <h5 class="font-weight-bold text-uppercase mb-4">Address</h5>
+        <ul class="list-unstyled">
+          <li>
+            <p>
+              <i class="fas fa-home ms-3"></i> SW1A, London, UK</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-envelope ms-3"></i> info@example.com</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-phone ms-3"></i> 01234567890</p>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 text-center mx-auto my-4">
+
+        <!-- Social buttons -->
+        <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
+
+        <a type="button" class="btn-floating btn-fb">
+            <a href="#" class="fa fa-facebook"></a>
+        </a>
+        <a type="button" class="btn-floating btn-tw">
+            <a href="#" class="fa fa-twitter"></a>
+        </a>
+        <a type="button" class="btn-floating btn-gplus">
+            <a href="#" class="fa fa-instagram"></a>
+        </a>
+
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+  </div>
+</footer>
+</html>
